235a0a5b84c6da5850222b215aa2e997e850aebd
Moritz <51033452+moritz158@users.noreply.github.com>
2020-03-13 17:11:56 +0100

    add Navigation

--- src/client/App.js
+++ src/client/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import { teal, amber } from '@material-ui/core/colors';
 
+import Navigation from './components/layout/Navigation';
 import Login from './components/Login';
 
 const theme = createMuiTheme({
@@ -22,6 +23,7 @@ export default class App extends Component {
     return (
       <div>
         <ThemeProvider theme={theme}>
+          <Navigation />
           <Login />
         </ThemeProvider>
       </div>
---
+++ src/client/components/layout/Navigation.js
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import HomeIcon from '@material-ui/icons/Home';
+import TimelineIcon from '@material-ui/icons/Timeline';
+import SettingsIcon from '@material-ui/icons/Settings';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import GroupIcon from '@material-ui/icons/Group';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+}));
+
+export default function Navigation() {
+  const classes = useStyles();
+
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = bool => (event) => {
+    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setOpen(bool);
+  };
+
+  const sideList = () => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        <ListItem button key="Home">
+          <ListItemIcon><HomeIcon /></ListItemIcon>
+          <ListItemText primary="Home" />
+        </ListItem>
+        <ListItem button key="Timeline">
+          <ListItemIcon><TimelineIcon /></ListItemIcon>
+          <ListItemText primary="Timeline" />
+        </ListItem>
+        <ListItem button key="Shopping">
+          <ListItemIcon><ShoppingCartIcon /></ListItemIcon>
+          <ListItemText primary="Shopping" />
+        </ListItem>
+        <ListItem button key="Friends">
+          <ListItemIcon><GroupIcon /></ListItemIcon>
+          <ListItemText primary="Friends" />
+        </ListItem>
+      </List>
+      <Divider />
+      <List>
+        <ListItem button key="Settings">
+          <ListItemIcon><SettingsIcon /></ListItemIcon>
+          <ListItemText primary="Settings" />
+        </ListItem>
+        <ListItem button key="Logout">
+          <ListItemIcon><ExitToAppIcon /></ListItemIcon>
+          <ListItemText primary="Logout" />
+        </ListItem>
+      </List>
+    </div>
+  );
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" onClick={() => setOpen(!open)} className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Project Outpost
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <SwipeableDrawer
+        open={open}
+        onClose={toggleDrawer(false)}
+        onOpen={toggleDrawer(true)}
+      >
+        {sideList('left')}
+      </SwipeableDrawer>
+    </div>
+  );
+}
