4ef351f70d5275ac77b722468173f3d7209bb5c8
moritz158 <51033452+moritz158@users.noreply.github.com>
2020-03-15 18:33:11 +0100

    move scanner to shopping page

--- src/client/pages/Shopping/Shopping.js
+++ src/client/pages/Shopping/Shopping.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import BarcodeScanner from '../../components/BarcodeScanner';
+
+function showBarcodeScannerResult(result) {
+  console.log(`Scanner Result: ${result}`);
+}
 
 export default function Shopping() {
   return (
     <div>
       <h1>Shopping</h1>
+      <BarcodeScanner callback={showBarcodeScannerResult} stopOnDetect stopOnClick />
     </div>
   );
 }
