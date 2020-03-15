e904e712436730eaf1c701e4a327520b62640920
moritz158 <51033452+moritz158@users.noreply.github.com>
2020-03-15 18:18:09 +0100

    react-router

--- src/client/App.js
+++ src/client/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import amber from '@material-ui/core/colors/amber';
 
 import Navigation from './components/layout/Navigation';
-import Login from './components/Login';
+import Login from './pages/Login/Login';
+import withAuth from './components/withAuth';
+
+import Home from './pages/Home/Home';
+import Timeline from './pages/Timeline/Timeline';
+import Shopping from './pages/Shopping/Shopping';
 
 const theme = createMuiTheme({
   palette: {
@@ -27,7 +33,13 @@ export default class App extends Component {
       <div>
         <ThemeProvider theme={theme}>
           <Navigation />
-          <Login />
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/" exact component={withAuth(Home)} />
+            <Route path="/timeline" component={withAuth(Timeline)} />
+            <Route path="/shopping" component={withAuth(Shopping)} />
+
+          </Switch>
         </ThemeProvider>
       </div>
     );
--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import App from './App';
+import RouterHistory from './Tools/RouterHistory';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router history={RouterHistory}><App /></Router>, document.getElementById('root'));
