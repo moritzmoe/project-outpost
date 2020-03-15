28e570232415f75ef587067b38b884cc19832675
moritz158 <51033452+moritz158@users.noreply.github.com>
2020-03-15 21:19:06 +0100

    Try it agaaaaaaaiin

--- src/server/index.js
+++ src/server/index.js
@@ -65,9 +65,9 @@ app.get('/api/logout', withAuth, (req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));// Handle React routing, return all requests to React app
+  app.use(express.static(path.join(__dirname, 'dist')));// Handle React routing, return all requests to React app
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
   });
 }
 
