0938c5f52675ff9b0dea3d8269241d28a2a9a125
moritz158 <51033452+moritz158@users.noreply.github.com>
2020-03-24 17:37:27 +0100

    add new endpoints to index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 
-const User = require('./models/user');
+const db = require('./config/database');
 
-const withAuth = require('./middleware');
+
+db
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 const app = express();
-const secret = 'mysecret';
 
 app.use(session({
   secret: 'secret',
@@ -28,44 +34,12 @@ app.get('/api/home', (req, res) => {
   res.send('Welcome!');
 });
 
-app.post('/api/register', (req, res) => {
-  const {
-    email, firstname, lastname, password
-  } = req.body;
-  console.log(req.body);
-  User.create({
-    email,
-    firstname,
-    lastname,
-    password
-  });
-  res.status(200).send('registered');
-});
+// Item routes
+app.use('/api/items', require('./routes/items'));
 
-app.post('/api/auth', (req, res) => {
-  const { email, password } = req.body;
-  User.findOne({ where: { email } }).then((user) => {
-    if (user === null) {
-      res.status(401).json({ error: 'Invalid username' });
-    } else if (!user.validPassword(password)) {
-      res.status(401).json({ error: 'Invalid password' });
-    } else {
-      const payload = { email };
-      const token = jwt.sign(payload, secret, {
-        expiresIn: '1h'
-      });
-      res.cookie('token', token, { httpOnly: true }).sendStatus(200);
-    }
-  });
-});
+// Authentication/Registration routes
+app.use('/api/auth', require('./routes/auth'));
 
-app.get('/api/checkToken', withAuth, (req, res) => {
-  res.sendStatus(200);
-});
-
-app.get('/api/logout', withAuth, (req, res) => {
-  res.clearCookie('token').sendStatus(200);
-});
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
@@ -74,7 +48,5 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.join('dist', 'index.html'));
   });
 }
-
-app.get('/some-route', (req, res) => { res.send('hello'); });
 
 app.listen(process.env.PORT || 8081, () => console.log(`Listening on port ${process.env.PORT || 8081}!`));
