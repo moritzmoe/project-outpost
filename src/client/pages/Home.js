d6abf74c0ff210fa2760f3ad00f84ee3f69e8b16
schneemax <51033307+schneemax@users.noreply.github.com>
2020-04-03 14:50:33 +0200

    Total Score added to homepage
    Die Homepage zeigt jetzt den summierten totalen Score an.

--- src/client/pages/Home.js
+++ src/client/pages/Home.js
@@ -1,14 +1,18 @@
 /* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import axios from 'axios';
 
 export default class Home extends React.Component {
   constructor() {
     super();
     // Set default message
     this.state = {
-      message: 'Loading...'
+      message: 'Loading...',
+      score: ''
     };
+    this.setScore();
   }
 
   componentDidMount() {
@@ -18,11 +22,28 @@ export default class Home extends React.Component {
       .then(res => this.setState({ message: res }));
   }
 
+  setScore = (evt) => {
+    // evt.preventDefault();
+    let totalScore = 0;
+    axios.get('/api/items').then((data) => {
+      data.data.map((item) => {
+        totalScore = parseInt(totalScore) + parseInt(item.score);
+      });
+      this.setState({ score: totalScore });
+    });
+  };
+
   render() {
     return (
       <Container maxWidth="sm">
         <h1>Home</h1>
         <p>{this.state.message}</p>
+        <Typography variant="h5" component="h2" gutterBottom>
+          Your total Score:
+        </Typography>
+        <Typography variant="h3" component="h2" gutterBottom>
+          {this.state.score}
+        </Typography>
       </Container>
     );
   }
