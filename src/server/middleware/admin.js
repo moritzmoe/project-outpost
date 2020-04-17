f1649cc98c6e5cb3ae204ab52f0f6cbfb9863852
moritz158 <51033452+moritz158@users.noreply.github.com>
2020-04-17 20:55:18 +0200

    create admin middleware

---
+++ src/server/middleware/admin.js
@@ -0,0 +1,26 @@
+/* eslint-disable func-names */
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.SECRET || 'pro_out_secret';
+
+const withAdmin = function (req, res, next) {
+  const { token } = req.cookies;
+
+  if (!token) {
+    res.status(401).send('Unauthorized: No token provided');
+  } else {
+    jwt.verify(token, secret, (err, decoded) => {
+      if (err) {
+        res.status(401).send('Unauthorized: Invalid token');
+      } else if (decoded.isAdmin) {
+        req.userId = decoded.id;
+        next();
+      } else {
+        res.status(401).send('Unauthorized: Admin Role required');
+      }
+    });
+  }
+};
+
+
+module.exports = withAdmin;
