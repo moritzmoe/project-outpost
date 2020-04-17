8207d030b1fb12ae5d982fc52da7785daeaabd40
moritz158 <51033452+moritz158@users.noreply.github.com>
2020-04-17 20:55:09 +0200

    move auth to middleware folder

---
+++ src/server/middleware/auth.js
@@ -0,0 +1,22 @@
+/* eslint-disable func-names */
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.SECRET || 'pro_out_secret';
+const withAuth = function (req, res, next) {
+  const { token } = req.cookies;
+
+  if (!token) {
+    res.status(401).send('Unauthorized: No token provided');
+  } else {
+    jwt.verify(token, secret, (err, decoded) => {
+      if (err) {
+        res.status(401).send('Unauthorized: Invalid token');
+      } else {
+        req.userId = decoded.id;
+        next();
+      }
+    });
+  }
+};
+
+module.exports = withAuth;
