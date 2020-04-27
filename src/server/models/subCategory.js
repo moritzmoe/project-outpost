98adba9f132802f3282ad923dc3ea0141cac20ef
moritz158 <51033452+moritz158@users.noreply.github.com>
2020-04-28 01:42:29 +0200

    introduce subCategories

---
+++ src/server/models/subCategory.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+
+const db = require('../config/database');
+
+const SubCategory = db.define('SubCatgory', {
+  parentCat: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    foreignKey: true
+  },
+  name: {
+    type: Sequelize.STRING(50),
+    allowNull: false
+  }
+});
+
+// create all the defined tables in the specified database.
+db.sync()
+  .then(() => console.log('SubCategory table has been successfully created, if one doesn\'t exist'))
+  .catch(error => console.log('While syncing the subCategory table this error occurred:', error));
+
+// export User model for use in other files.
+module.exports = SubCategory;
