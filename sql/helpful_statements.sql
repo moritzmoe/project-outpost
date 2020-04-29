432891ab6de760c2eb75458fe8e8fb55a74868b8
moritz158 <51033452+moritz158@users.noreply.github.com>
2020-04-29 22:26:58 +0200

    add table drop cascade to helpful sql

--- sql/helpful_statements.sql
+++ sql/helpful_statements.sql
@@ -3,3 +3,15 @@ SELECT sc.id, sc.name AS subcategory, c.name AS parentcategory
 FROM "SubCatgories"  AS sc
 INNER JOIN "Categories" AS c
 ON (sc."parentCat" = c.id);
+
+
+-- drop all tables
+DROP TABLE "Categories" CASCADE;
+DROP TABLE "Items" CASCADE;
+DROP TABLE "PackMats" CASCADE;
+DROP TABLE "PackTypes" CASCADE;
+DROP TABLE "Purchases" CASCADE;
+DROP TABLE "SubCategories" CASCADE;
+DROP TABLE "Users" CASCADE;
+DROP TABLE "purchaseItem" CASCADE;
+
