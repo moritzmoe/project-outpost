6d253ae2b0e7c8abf6c205449f61bef0f558569c
moritz158 <51033452+moritz158@users.noreply.github.com>
2020-05-09 19:18:23 +0200

    change associations in datamodel

--- src/server/config/associations.js
+++ src/server/config/associations.js
@@ -1,30 +1,10 @@
 const Category = require('../models/category');
 const Item = require('../models/item');
-const PackMat = require('../models/packMat');
-const PackType = require('../models/packType');
+const Packaging = require('../models/packaging');
+const Origin = require('../models/origin');
 const Purchase = require('../models/purchase');
 const SubCategory = require('../models/subCategory');
 const User = require('../models/user');
-
-// User created item 1:n
-User.hasMany(Item, {
-  as: 'created',
-  foreignKey: 'createdBy'
-});
-Item.belongsTo(User, {
-  as: 'created',
-  foreignKey: 'createdBy'
-});
-
-// User last updated item 1:n
-User.hasMany(Item, {
-  as: 'lastUpdated',
-  foreignKey: 'lastUpdatedBy'
-});
-Item.belongsTo(User, {
-  as: 'lastUpdated',
-  foreignKey: 'lastUpdatedBy'
-});
 
 // User has many Purchases 1:n
 User.hasMany(Purchase, {
@@ -42,36 +22,36 @@ Item.belongsToMany(Purchase, {
   through: 'purchaseItem'
 });
 
-// Item has (sub)category 1:n
-SubCategory.hasMany(Item, {
-  foreignKey: 'categoryId'
+// User created item 1:n
+Item.belongsTo(User, {
+  as: 'created',
+  foreignKey: 'createdBy'
 });
-Item.belongsTo(SubCategory, {
-  foreignKey: 'categoryId'
+
+// User last updated item 1:n
+Item.belongsTo(User, {
+  as: 'lastUpdated',
+  foreignKey: 'lastUpdatedBy'
 });
 
 // Category has SubCategories 1:n
 Category.hasMany(SubCategory, {
   foreignKey: 'parentCat'
 });
-SubCategory.belongsTo(Category, {
-  foreignKey: 'parentCat'
+
+// Item has (sub)category 1:n
+Item.belongsTo(SubCategory, {
+  foreignKey: 'categoryId'
 });
 
-// PackMat can have many items 1:n
-PackMat.hasMany(Item, {
-  foreignKey: 'packmat'
-});
-Item.belongsTo(PackMat, {
-  foreignKey: 'packmat'
+// Items has packaging 1:n
+Item.belongsTo(Packaging, {
+  foreignKey: 'packaging'
 });
 
-// PackType can have many items 1:n
-PackType.hasMany(Item, {
-  foreignKey: 'packtype'
-});
-Item.belongsTo(PackType, {
-  foreignKey: 'packtype'
+// Item has origin
+Item.belongsTo(Origin, {
+  foreignKey: 'origin'
 });
 
 require('./initialData');
