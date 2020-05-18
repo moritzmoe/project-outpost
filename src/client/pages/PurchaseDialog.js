1ed398a56bb6c0e945e496e6a4b59b307f04d75e
moritz158 <51033452+moritz158@users.noreply.github.com>
2020-05-18 13:23:17 +0200

    fix scanner closing issue

--- src/client/pages/PurchaseDialog.js
+++ src/client/pages/PurchaseDialog.js
@@ -122,6 +122,8 @@ export default function PurchaseDialog(props) {
       setErrorMsg(err.response.data.error);
       setError(true);
     });
+    handleBarcodeDialogClose();
+    handleBarcodeTypeInClose();
   };
 
   const nothing = () => {
