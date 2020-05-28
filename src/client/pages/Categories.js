9c27a7ebf819e819c78b6953b05f02386238021c
schneemax <51033307+schneemax@users.noreply.github.com>
2020-05-28 12:37:34 +0200

    Added Material Table
    Added Material Table
    Installed Material Table for that and installed: npm i @date-io/date-fns date-fns@next
    to get rid of error.

--- src/client/pages/Categories.js
+++ src/client/pages/Categories.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useSetStoreValue, useStoreValue } from 'react-context-hook';
-import { makeStyles } from '@material-ui/core/styles';
 import axios from 'axios';
+
+import { makeStyles } from '@material-ui/core/styles';
+import MaterialTable from 'material-table';
+
 // Only for testing purposes
 import Container from '@material-ui/core/Container';
 
@@ -25,6 +28,8 @@ export default function Categories() {
   const setPageName = useSetStoreValue('pageName');
   const isAdmin = useStoreValue('isAdmin');
 
+  const [state, setState] = React.useState({});
+
   useEffect(() => {
     setPageName('Category Database');
   }, []);
@@ -33,9 +38,45 @@ export default function Categories() {
     <div>
       {isAdmin ? (
         <Container>
-          <Button variant="contained" color="primary">
-            Primary
-          </Button>
+          <MaterialTable
+            title="Editable Example"
+            columns={state.columns}
+            data={state.data}
+            editable={{
+              onRowAdd: newData => new Promise((resolve) => {
+                setTimeout(() => {
+                  resolve();
+                  setState((prevState) => {
+                    const data = [...prevState.data];
+                    data.push(newData);
+                    return { ...prevState, data };
+                  });
+                }, 600);
+              }),
+              onRowUpdate: (newData, oldData) => new Promise((resolve) => {
+                setTimeout(() => {
+                  resolve();
+                  if (oldData) {
+                    setState((prevState) => {
+                      const data = [...prevState.data];
+                      data[data.indexOf(oldData)] = newData;
+                      return { ...prevState, data };
+                    });
+                  }
+                }, 600);
+              }),
+              onRowDelete: oldData => new Promise((resolve) => {
+                setTimeout(() => {
+                  resolve();
+                  setState((prevState) => {
+                    const data = [...prevState.data];
+                    data.splice(data.indexOf(oldData), 1);
+                    return { ...prevState, data };
+                  });
+                }, 600);
+              }),
+            }}
+          />
         </Container>
       ) : ''}
     </div>
